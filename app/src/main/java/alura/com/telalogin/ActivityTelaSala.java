a8ecc7295a0980170b8972ff9e8e152b930f90b9
Ygor Nogatz <ynogatzds@gmail.com>
2020-02-14 09:34:40 -0300

    calendario adicionado ao floating action button

--- app/src/main/java/alura/com/telalogin/ActivityTelaSala.java
+++ app/src/main/java/alura/com/telalogin/ActivityTelaSala.java
@@ -1,18 +1,24 @@
 package alura.com.telalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import alura.com.modelo.Sala;
 
-public class ActivityTelaSala extends AppCompatActivity {
+public class ActivityTelaSala extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +53,26 @@ public class ActivityTelaSala extends AppCompatActivity {
             }
         }));
 
-        FloatingActionButton fabAdicionarReservar = (FloatingActionButton) findViewById(R.id.fab_adicionar_reserva);
-        fabAdicionarReservar.setOnClickListener((new View.OnClickListener() {
+        FloatingActionButton fabSelecionarDia = (FloatingActionButton) findViewById(R.id.fab_adicionar_reserva);
+        fabSelecionarDia.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent it = new Intent(ActivityTelaSala.this, ActivityTelaReservarSala.class);
-                startActivity(it);
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+
             }
-        }));
+        });
+
+    }
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView textView = (TextView) findViewById(R.id.tv_dia_selecionado);
+        textView.setText(currentDateString);
     }
 }
