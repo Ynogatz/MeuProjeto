c3f0d6f4e326f0db6baab0d144e22a9290859993
Ygor Nogatz <ynogatzds@gmail.com>
2020-03-06 11:28:55 -0300

    back button tela mais informações reserva arrumado

--- app/src/main/java/alura/com/telalogin/ActivityTelaInfoReserva.java
+++ app/src/main/java/alura/com/telalogin/ActivityTelaInfoReserva.java
@@ -42,4 +42,11 @@ public class ActivityTelaInfoReserva extends AppCompatActivity {
         descricao.setText(reserva.getDescricao());
 
     }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
